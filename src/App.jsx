5aab9b00fe1e1a74480d63990a0e7a70c55daa6f
Ahmad Yar <ahmadyar228@gmail.com>
2025-11-05 12:20:36 +0500

    public assets for movie project

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,14 @@ function App() {
   return (
     <main>
       <div className='pattern' />
-      
+
       <div className='wrapper'>
         <header>
-          <h1>Find Movies you will enjoy</h1>
+          <img src="./hero.png" alt='Hero Banner' />
+          <h1>
+            Find <span className='text-gradient'>Movies</span>You will enjoy without hassel
+          </h1>
+          <p>Search</p>
         </header>
       </div>
     </main>
