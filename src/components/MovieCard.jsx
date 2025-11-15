692fc24c26359c32e686a6724142a089f6a5a5ef
Ahmad Yar <ahmadyar228@gmail.com>
2025-11-15 18:44:05 +0500

    vote average in movie component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,10 +10,25 @@ const MovieCard = ({movie: {
   return (
     <div className="movie-card">
       <img
-        src={poster_path ? 
-          `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-movie.png'}
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500${poster_path}`
+            : "/no-movie.png"
+        }
         alt={title}
       />
+      
+      <div className="mt-4">
+        <h3>{title}</h3>
+
+        <div className="conten">
+          <div className="rating">
+            <img src="star.svg" alt="Star Icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A" }</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 };
